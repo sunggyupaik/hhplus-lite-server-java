ea278077d3f5fe8d0ef001975b0f32d1efbeacb3
sunggyupaik <melon9751@naver.com>
2025-12-02 22:56:05 +0900

    [feat] 예외 상황을 추가하라
    공통 예외를 정의하며 예외 클래스를 추가합니다.
    또한 공통 예외 응답을 만듭니다

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	// Lombok 라이브러리
 	compileOnly("org.projectlombok:lombok:1.18.28")
 	annotationProcessor("org.projectlombok:lombok:1.18.28")
